0c30e930d034c17aa93c2631a94816304af1fd0b
nicholas kiprono <kiprononicholas11@gmail.com>
2024-05-08 14:54:11 +0300

    just added a new file

---
+++ assignment.py
@@ -0,0 +1,19 @@
+
+# music = "hiphop","regge","bongo","rnb"
+# jmusic ="-".join(music)
+# print(jmusic)
+
+# smusic =jmusic.split("-")
+# print(smusic)
+
+
+
+music_list = "hiphop","regge","bongo","rnb","genge","gospel"
+joined_music = "'".join(music_list)
+print("joined_music:",joined_music)
+
+
+split_music = joined_music.split("'")
+print(split_music)
+
+
